0afd30ed0bcc2b607af34b46e138a25fdd047dea
Thomas Levine <_@thomaslevine.com>
2014-11-23 04:51:58 +0000

    -

--- cbuh/index.py
+++ cbuh/index.py
@@ -17,20 +17,20 @@ def index(contacts, database, prefixes):
     db = xapian.WritableDatabase(database, xapian.DB_CREATE_OR_OPEN)
 
     p = set()
-    for section in c.sections():
+    for person in c.sections():
         doc = xapian.Document()
         termgenerator.set_document(doc)
 
-        termgenerator.index_text(section, 1, u'id')
-        for prefix, content in c.items(section):
+        termgenerator.index_text(person, 1, u'id')
+        for prefix, content in c.items(person):
             termgenerator.index_text(content, 1, u'X' + prefix)
             termgenerator.index_text(content)
             termgenerator.increase_termpos()
             p.add(prefix)
 
-        doc.add_boolean_term(section)
-        doc.set_data(section)
-        db.replace_document(section, doc)
+        doc.add_boolean_term(u'Q' + person)
+        doc.set_data(person)
+        db.replace_document(u'Q' + person, doc)
 
     with open(prefixes, 'wb') as fp:
         json.dump(list(p), fp)
